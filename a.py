b4002a77efd4604d7f0adb162236c36bbf8cdc81
Carlaav <carla2002.av@gmail.com>
2022-03-17 12:42:23 +0100

    a

--- a.py
+++ a.py
@@ -1,1 +1,16 @@
-
+class Punto2D:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+    def translacion(self,a,b):
+        self.x=self.x+a
+        self.y=self.y+b
+class Punto3D:
+    def __init__(self,x,y,z):
+        self.punto2d=Punto2D(x,y)
+        self.z=z
+    def translacion(self,a,b,c):
+        self.punto2d.x=self.punto2d.x+a
+        self.punto2d.y=self.punto2d.y+b
+        self.z=self.z+c
+        print("x:",self.punto2d.x, "y:",self.punto2d.y,"z:",self.z)
--- b.py
+++ b.py
@@ -1,1 +1,8 @@
-
+#base.A() devuelve "a"
+#derivada.A() devuelve "a"
+#print() no devuelve nada
+#base.B() devuelve "b"
+#derivada.B() devuelve "bb" y "bb"
+#base.C() devuelve "c"
+#derivada.C() devuelve "cc"
+#derivada = base y derivada.C() devuelve c
